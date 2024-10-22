48c90fd85b5423732cc7ce84505d2114feecd363
Evgeny Korotaev <e.korotaev@robo.finance>
2024-10-22 23:18:06 +0700

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,42 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+
+  devtools: {
+    enabled: true,
+  },
+
+  modules: [
+    '@nuxtjs/i18n',
+  ],
+
+  // https://i18n.nuxtjs.org/docs/getting-started/usage
+  i18n: {
+    baseUrl: process.env.APP_BASE_URL,
+    strategy: 'no_prefix',
+    defaultLocale: 'pt',
+    langDir: './locales/',
+
+    locales: [
+      {
+        code: 'en',
+        language: 'en-US',
+        name: 'English',
+        file: 'en.js',
+      },
+      {
+        code: 'pt',
+        language: 'pt-BR',
+        name: 'Português',
+        file: 'pt.js',
+      },
+    ],
+
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieKey: 'i18n_redirected',
+    },
+
+    vueI18n: './i18n.config.ts',
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<script setup>
+const { t, setLocale } = useI18n();
+</script>
+
+<template>
+  <div>
+    <div>
+      <button
+        type="button"
+        @click="setLocale('en')"
+      >
+        en
+      </button>
+
+      <button
+        type="button"
+        @click="setLocale('pt')"
+      >
+        pt
+      </button>
+
+      <p v-text="t('seo.common.title')" />
+
+      <I18nT
+        keypath="seo.common.title"
+        tag="p"
+      />
+    </div>
+  </div>
+</template>
