03c69b1ee19eef63f69b93d1fd75c7d12f318513
Evgeny Korotaev <e.korotaev@robo.finance>
2025-11-04 17:00:02 +0700

    Переменные окружения для supabase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,8 +24,8 @@ export default defineNuxtConfig({
       },
 
       supabase: {
-        url: process.env.SUPABASE_URL,
-        key: process.env.SUPABASE_KEY,
+        url: '',
+        key: '',
       },
     },
   },
