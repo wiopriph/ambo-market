1bb282967b9094b05e3db9589191b22ce621522a
Evgeny Korotaev <e.korotaev@robo.finance>
2026-06-05 12:47:41 +0700

    Стили для breadcrumb

--- app.config.ts
+++ app.config.ts
@@ -4,5 +4,16 @@ export default defineAppConfig({
       primary: 'blue',
       // neutral: 'slate',
     },
+
+    breadcrumb: {
+      slots: {
+        root: 'mb-4',
+        list: 'flex items-center gap-1 min-w-0',
+        item: 'shrink-0 last:flex-1 last:min-w-0',
+        link: 'text-xs md:text-sm hover:text-primary aria-[current=page]:pointer-events-none',
+        linkLabel: 'block whitespace-nowrap overflow-hidden text-ellipsis truncate aria-[current=page]:font-medium',
+        separator: 'mx-1 text-gray-400',
+      },
+    },
   },
 });
