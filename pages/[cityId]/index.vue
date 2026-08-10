72ab861f3f72af7205080e980cbbf9d4cf893f80
Evgeny Korotaev <e.korotaev@robo.finance>
2026-08-10 21:19:39 +0700

    Canonical на всех страницах
    Раньше canonical был только на /all. Теперь глобальный плагин ставит
    self-canonical везде: /all канонизируется на /, из query сохраняется
    только page (при page > 1), фильтры и поиск канонизируются на чистый
    URL листинга. Локальный canonical из страницы города убран.

--- pages/[cityId]/index.vue
+++ pages/[cityId]/index.vue
@@ -58,14 +58,6 @@ const seo = computed(() => {
   };
 });
 
-const config = useRuntimeConfig();
-
-const canonicalLink = computed(() =>
-  cityId.value === 'all' ?
-    [{ rel: 'canonical', href: config?.public?.appBaseUrl }] :
-    [],
-);
-
 useHead({
   title: seo.value.title,
   meta: [
@@ -75,7 +67,6 @@ useHead({
     { key: 'og:description', property: 'og:description', content: seo.value.description },
     { key: 'twitter:description', property: 'twitter:description', content: seo.value.description },
   ],
-  link: canonicalLink.value,
 });
 
 const categories = computed(() => CATEGORIES.map(category => ({
